c52ed45075ff65918920f696d8ef9766b8028a2d
Pete Fowler <fowler.pete@gmail.com>
2022-08-29 15:09:49 -0600

    commit tree 8ea61f52770824c59fbd531cc282f73b68d674e1

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id='about'>
+      <h2>About</h2>
+    </div>
+  )
+  // <About> component should return a <div> with an id of about. To match the screenshot, the div should contain an <h2> element with the text "About." However, as long as it has an id of about, it will pass the test!
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      {<Home />}
+      {<About />}
     </div>
   );
 }
